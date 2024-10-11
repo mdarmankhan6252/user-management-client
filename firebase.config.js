d71c14d729c660b585899745eefb516e12dab103
mdarmankhan6252 <mdarmankhan6252@gmail.com>
2024-10-11 23:00:02 +0600

    i modifyed firebase env veriable file

--- firebase.config.js
+++ firebase.config.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBVucf7YVr03CKFFFEPy2aS42ZXg9ZNGDo",
-  authDomain: "user-management-system-298a9.firebaseapp.com",
-  projectId: "user-management-system-298a9",
-  storageBucket: "user-management-system-298a9.appspot.com",
-  messagingSenderId: "564280653892",
-  appId: "1:564280653892:web:d266a38301f16765396411"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
